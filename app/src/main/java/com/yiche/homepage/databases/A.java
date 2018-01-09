77bdbc402e28bd31aed9bae6664fb68402f325d0
hanbo <hanbov5@gmail.com>
2018-01-09 18:35:56 +0800

    no message

---
+++ app/src/main/java/com/yiche/homepage/databases/A.java
@@ -0,0 +1,10 @@
+package com.yiche.homepage.databases;
+
+/**
+ * Created on 2018/1/8 18: 38.
+ *
+ * @author lyongwang
+ */
+
+public class A {
+}
---
+++ app/src/main/java/com/yiche/homepage/serviceimplements/A.java
@@ -0,0 +1,10 @@
+package com.yiche.homepage.serviceimplements;
+
+/**
+ * Created on 2018/1/8 18: 38.
+ *
+ * @author lyongwang
+ */
+
+public class A {
+}
